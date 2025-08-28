573be23aea713862df8bb600093742452474d99c
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-08-29 00:00:31 +0300

    Create extra_spawns.py

---
+++ ballsdex/packages/countryballs/extra_spawns.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+import asyncio
+import logging
+import random
+from ballsdex.core.models import Ball, Special
+from ballsdex.packages.countryballs.countryball import BallSpawnView
+
+if TYPE_CHECKING:
+  from ballsdex.core.bot import BallsDexBot
+
+log = logging.getLogger("ballsdex.packages.countryballs.extra_spawns")
+
+async def pay_to_win_spawner(self, bot: "BallsDexBot"):
+    channel_id = 1391136498769723432
+    while True:
+        special_obj = ""
+        options = [None, "Brawl Pass", "Brawl Pass Plus"]
+        weights = [40, 40, 20]
+        picked_special = random.choices(options, weights=weights, k=1)[0]
+        if picked_special == None:
+            pass
+        else:
+            special_obj = await Special.get(name=str(picked_special))
+        try:
+             ball = await BallSpawnView.get_random(bot)
+             ball.special = special_obj
+             await ball.spawn(bot.get_channel(channel_id))
+
+        except Exception as e:
+            log.error(f"An error occurred (P2W): {e}")
+
+        await asyncio.sleep(180)
+
+async def basic_spawner(self, bot: "BallsDexBot"):
+    channel_id = 1295410565765922862
+    while True:
+        try:
+             ball = await BallSpawnView.get_random(bot)
+             await ball.spawn(bot.get_channel(channel_id))
+
+        except Exception as e:
+            log.error(f"An error occurred (Basic): {e}")
+
+        await asyncio.sleep(300)
