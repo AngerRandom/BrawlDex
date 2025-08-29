1fd28bfc7142a6dd862fe27ae3be0d319d39f5de
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-08-29 22:46:21 +0300

    Update models.py

--- admin_panel/bd_models/models.py
+++ admin_panel/bd_models/models.py
@@ -168,6 +168,7 @@ class Player(models.Model):
 class Economy(models.Model):
     name = models.CharField(max_length=64)
     icon = models.ImageField(max_length=200, help_text="512x512 PNG image")
+    emoji = models.BigIntegerField(help_text="Emoji ID for this economy")
 
     def __str__(self) -> str:
         return self.name
