1ff2101b37850ececeb22c05e2f4084c4859d413
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-08-31 20:00:09 +0300

    Update extra_spawns.py

--- ballsdex/packages/countryballs/extra_spawns.py
+++ ballsdex/packages/countryballs/extra_spawns.py
@@ -11,7 +11,30 @@ if TYPE_CHECKING:
 log = logging.getLogger("ballsdex.packages.countryballs.extra_spawns")
 
 async def pay_to_win_spawner(self):
-    channel_id = 1391136498769723432
+    channel = self.get_channel(1391136498769723432)
+    spawn_time = 0
+    spawn_amount = 0
+    boost_count = channel.guild.premium_subscription_count
+    if boost_count <= 15:
+      spawn_time = 15*60
+      spawn_amount = 1
+    elif boost_count == 16:
+      spawn_time = 14*60
+      spawn_amount = 1
+    elif boost_count == 17:
+      spawn_time = 13*60
+      spawn_amount = 2
+    elif boost_count == 18:
+      spawn_time = 12*60
+      spawn_amount = 2
+    elif boost_count == 19:
+      spawn_time = 11*60
+      spawn_amount = 2
+    elif boost_count >= 20:
+      spawn_time = 10*60
+      spawn_amount = 3
+    else:
+      pass
     while True:
         special_obj = ""
         options = [None, "Brawl Pass", "Brawl Pass Plus"]
@@ -22,23 +45,48 @@ async def pay_to_win_spawner(self):
         else:
             special_obj = await Special.get(name=str(picked_special))
         try:
-             ball = await BallSpawnView.get_random(self)
-             ball.special = special_obj
-             await ball.spawn(self.get_channel(channel_id))
+            for i in range(spawn_amount):
+                ball = await BallSpawnView.get_random(self)
+                ball.special = special_obj
+                await ball.spawn(channel)
 
         except Exception as e:
             log.error(f"An error occurred (P2W): {e}")
 
-        await asyncio.sleep(600)
+        await asyncio.sleep(spawn_time)
 
 async def basic_spawner(self):
-    channel_id = 1295410565765922862
+    channel = self.get_channel(1295410565765922862)
+    spawn_time = 0
+    spawn_amount = 0
+    boost_count = channel.guild.premium_subscription_count
+    if boost_count <= 15:
+      spawn_time = 15*60
+      spawn_amount = 1
+    elif boost_count == 16:
+      spawn_time = 14*60
+      spawn_amount = 1
+    elif boost_count == 17:
+      spawn_time = 13*60
+      spawn_amount = 2
+    elif boost_count == 18:
+      spawn_time = 12*60
+      spawn_amount = 2
+    elif boost_count == 19:
+      spawn_time = 11*60
+      spawn_amount = 2
+    elif boost_count >= 20:
+      spawn_time = 10*60
+      spawn_amount = 3
+    else:
+      pass
     while True:
         try:
-             ball = await BallSpawnView.get_random(self)
-             await ball.spawn(self.get_channel(channel_id))
+            for i in range(spawn_amount):
+                ball = await BallSpawnView.get_random(self)
+                await ball.spawn(channel)
 
         except Exception as e:
             log.error(f"An error occurred (Basic): {e}")
 
-        await asyncio.sleep(600)
+        await asyncio.sleep(spawn_time)
